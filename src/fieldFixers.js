0cef03b978dd19504ed1ffb0ce3888b2d064860b
Micael Galvan <micael.galvan@visualvault.com>
2024-05-17 16:52:11 -0300

    .

--- src/fieldFixers.js
+++ src/fieldFixers.js
@@ -1,48 +1,91 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-param-reassign */
+const xlsx = require("xlsx");
+
 /* -------------------------------------------------------------------------- */
 /*                              GLOBAL VARIABLES                              */
 /* -------------------------------------------------------------------------- */
+
+// Field type mapping dictionary
+const fieldTypeMapping = {
+    FieldContainer: "Container",
+    FieldCheckbox: "Checkbox Control",
+    ImageFormControl: "Image Control",
+    FieldLabel: "Label Control",
+    FieldTextbox3: "Textbox Control",
+    FormButton: "Form Button Control",
+    FieldDropDownList3: "DropDownList Control",
+    FieldTextArea3: "Multiline Textbox Control",
+    CellField: "Cell Field Control",
+    FieldCalendar3: "Calendar Control",
+    UploadButton: "Upload Button Control",
+    FormIDStamp: "Form ID Stamp Control",
+    RepeatingRowControl: "Repeating Row Control",
+    FieldDataGrid: "Data Grid Control",
+    UserIDStamp: "Signature Stamp Control",
+    QuestionsControl: "Question Control",
+    SumField: "Sum Field Control",
+    FieldRectangle: "Rectangle",
+    FieldCommentList: "Conversation",
+    FieldSlider: "Slider Control",
+    BarCodeFormControl: "Barcode Image",
+};
 
 /* -------------------------------------------------------------------------- */
 /*                              HELPER FUNCTIONS                              */
 /* -------------------------------------------------------------------------- */
 
 function getFieldName(form, pIndex, fieldIndex) {
-  const fields = getPageFields(form, pIndex);
-  const field = fields[fieldIndex];
-  const fieldName = field.Name[0];
-  return fieldName;
+    const fields = getPageFields(form, pIndex);
+    const field = fields[fieldIndex];
+    const fieldName = field.Name[0];
+    return fieldName;
 }
 
 function getPageFields(form, pIndex) {
-  return form.FormPages[pIndex].FormPage[0].FieldList[0].BaseField;
+    return form.FormPages[pIndex].FormPage[0].FieldList[0].BaseField;
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                MAIN FUNCTION                               */
 /* -------------------------------------------------------------------------- */
 
-function fixFields(form) {
-  try {
-    form.FormPages.forEach((page, pageIndex) => {
-      const fields = getPageFields(form, pageIndex);
+function fixFields(form, desiredFieldTypes, formName) {
+    const fields = [];
 
-      // Iterate over each field in the page
-      fields.forEach((field, fieldIndex) => {
-        const fieldType = field.$["xsi:type"];
+    try {
+        form.FormPages.forEach((page, pageIndex) => {
+            const pageFields = getPageFields(form, pageIndex);
 
-        const fieldName = getFieldName(form, pageIndex, fieldIndex);
-        console.log(`${fieldName} : ${fieldType}`);
-      
-      });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  return form;
+            // Iterate over each field in the page
+            pageFields.forEach((field, fieldIndex) => {
+                const fieldType = field.$["xsi:type"];
+                const fieldName = getFieldName(form, pageIndex, fieldIndex);
+
+                if (desiredFieldTypes.length === 0 || desiredFieldTypes.includes(fieldType)) {
+                    fields.push({
+                        name: fieldName,
+                        type: fieldType,
+                        formName: formName
+                    });
+                }
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+    return fields;
+}
+
+function generateExcelFile(excelFileName, fields) {
+    const workbook = xlsx.utils.book_new();
+    const worksheetData = fields.map((field) => [field.formName, field.name, "", fieldTypeMapping[field.type] || field.type]);
+
+    worksheetData.unshift(["VV TABLE NAME / RECORD TYPE", "VV FIELD NAME", "VV DB DATA TYPE", "VV FORM DATA TYPE"]);
+
+    const worksheet = xlsx.utils.aoa_to_sheet(worksheetData);
+    xlsx.utils.book_append_sheet(workbook, worksheet, "Data Dictionary");
+    xlsx.writeFile(workbook, `./public/output/${excelFileName}.xlsx`);
 }
 
 module.exports = fixFields;
+module.exports.generateExcelFile = generateExcelFile;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-/* eslint-disable no-restricted-syntax */
+require("dotenv").config();
 const fs = require("fs");
+const path = require("path");
 const xml2js = require("xml2js");
 const fixFields = require("./fieldFixers");
-const {
-  addToReport,
-  report,
-  clearReport,
-  generateReport,
-} = require("./report");
 
 /* -------------------------------------------------------------------------- */
 /*                              CONFIG VARIABLES                              */
@@ -15,94 +10,61 @@ const {
 
 // XML file paths
 const inputXmlFolderPath = "./public/input/";
-const outputXmlPath = "./public/output/";
-const outputReportPath = "./public/output/";
+
+// Load desired field types from .env file or use default values
+const desiredFieldTypes = process.env.DESIRED_FIELD_TYPES;
+const outputExcelName = process.env.OUTPUT_EXCEL_NAME;
 
 /* -------------------------------------------------------------------------- */
 /*                                FILE HANDLING                               */
 /* -------------------------------------------------------------------------- */
 
 function getAllXMLFilePaths(path) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(path, (err, files) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      // Filter out non-XML files
-      const xmlFiles = files.filter((file) => file.endsWith(".xml"));
-      resolve(xmlFiles);
+    return new Promise((resolve, reject) => {
+        fs.readdir(path, (err, files) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            // Filter out non-XML files
+            const xmlFiles = files.filter((file) => file.endsWith(".xml"));
+            resolve(xmlFiles);
+        });
     });
-  });
 }
 
 function readXmlFile(inputXmlPath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(inputXmlPath, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(data);
+    return new Promise((resolve, reject) => {
+        fs.readFile(inputXmlPath, "utf-8", (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
     });
-  });
 }
-
-function writeFile(path, content) {
-  return new Promise((resolve, reject) => {
-    // Write the file
-    fs.writeFile(path, content, (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      console.log("The file has been saved!");
-      resolve();
-    });
-  });
-}
-
-/* -------------------------------------------------------------------------- */
-/*                              HELPER FUNCTIONS                              */
-/* -------------------------------------------------------------------------- */
 
 function convertXmlToJson(data) {
-  return new Promise((resolve, reject) => {
-    xml2js.parseString(data, (err, result) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(result);
+    return new Promise((resolve, reject) => {
+        xml2js.parseString(data, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(result);
+        });
     });
-  });
 }
 
-function convertJsonToXml(json) {
-  const builder = new xml2js.Builder();
-  const xml = builder.buildObject(json);
-  return xml;
-}
+async function processXmlFile(filePath) {
+    const data = await readXmlFile(path.join(inputXmlFolderPath, filePath));
+    const fileName = path.basename(filePath, ".xml");
+    const jsonData = await convertXmlToJson(data);
+    const formEntity = jsonData.FormEntity;
 
-async function processXmlFile(path) {
-  const data = await readXmlFile(inputXmlFolderPath + path);
-  const fileName = path.split(".")[0];
-  const jsonData = await convertXmlToJson(data);
-  const formEntity = jsonData.FormEntity;
-
-  // Analyze the XML and fix the fields
-  jsonData.FormEntity = fixFields(formEntity);
-
-  // Convert the JSON back to XML
-  const xml = convertJsonToXml(jsonData);
-
-  // Create files
-  const xmlPath = `${outputXmlPath + fileName}.xml`;
-  const reportPath = `${outputReportPath + fileName}.md`;
-  const reportData = await generateReport(report, fileName);
-  await writeFile(reportPath, reportData);
-  await writeFile(xmlPath, xml);
-  clearReport();
+    // Extract fields using fixFields function
+    return fixFields(formEntity, desiredFieldTypes, fileName);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -110,17 +72,23 @@ async function processXmlFile(path) {
 /* -------------------------------------------------------------------------- */
 
 async function main() {
-  try {
-    const filePaths = await getAllXMLFilePaths(inputXmlFolderPath);
+    try {
+        const filePaths = await getAllXMLFilePaths(inputXmlFolderPath);
+        const allFields = [];
 
-    for (const path of filePaths) {
-      // eslint-disable-next-line no-await-in-loop
-      await processXmlFile(path);
+        for (const filePath of filePaths) {
+            // eslint-disable-next-line no-await-in-loop
+            const fields = await processXmlFile(filePath);
+            allFields.push(...fields);
+        }
+
+        // Generate the Excel file with all accumulated fields
+        fixFields.generateExcelFile(outputExcelName, allFields);
+
+        console.log("All files have been processed!");
+    } catch (error) {
+        console.log(error);
     }
-    console.log("All files have been processed!");
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 main();
